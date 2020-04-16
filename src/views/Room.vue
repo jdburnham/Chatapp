8e78d037752fee0aa36d2de7f02d717b4bc0c495
jdburnham <jburnham@stu.southtexascollege.edu>
2020-04-16 16:30:40 -0500

    Finished App

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -3,23 +3,23 @@
     <v-navigation-drawer app permanent clipped color="grey lighten-4">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title"> {{ room.name }} </v-list-item-title>
+          <v-list-item-title class="title">{{ room.name }}</v-list-item-title>
           <v-list-item-subtitle>Room</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider />
       <v-list nav>
-        <v-list-item v-for="room in rooms" :key="room.id" :to="`/room/${room.id}`">
+        <v-list-item v-for="r in rooms" :key="r.id" :to="`/room/${r.id}`">
           <v-list-item-content>
-            <v-list-item-titel>{{ room.name }}</v-list-item-titel>
+            <v-list-item-title>{{ r.name }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-container>
       <div v-for="message in messages" :key="message.id">
-        <p class="overline">{{ message.author }} on {{ message.createdAt.toLocaleString() }}</p>
-        <p>{{ message.text }}</p>
+        <p class="overline">{{ message.author }} on {{ message.createdAt.toDate().toLocaleString() }}</p>
+        <p>{{ message.message }}</p>
       </div>
     </v-container>
     <v-footer absolute color="grey lighten-4">
@@ -30,38 +30,61 @@
 </template>
 
 <script>
+import { db } from '../plugins/firebase'
+import { mapGetters } from 'vuex'
 export default {
   name: 'Room',
   data() {
     return {
       newMessage: '',
-      room: {
-        id: 123,
-        name: 'Room Test',
-        createdAt: new Date(),
-      },
-      rooms: [
-        { id: 1, name: 'Room1' },
-        { id: 2, name: 'Room2' },
-      ],
-      messages: [
-        {
-          id: 1,
-          text: 'First message',
-          author: 'Josh Burnham',
-          createdAt: new Date(),
-        },
-        {
-          id: 2,
-          text: 'Games are fun',
-          author: 'Other Bro',
-          createdAt: new Date(),
-        },
-      ],
+      room: {},
+      rooms: [],
+      messages: [],
     }
   },
+  computed: {
+    ...mapGetters({
+      user: 'getUser',
+    }),
+  },
+  watch: {
+    '$route.params.id': async function() {
+      await this.$bind('room', db.collection('rooms').doc(this.$route.params.id))
+      await this.$bind(
+        'messages',
+        db
+          .collection('messages')
+          .where('roomId', '==', this.$route.params.id)
+          .orderBy('createdAt'),
+      )
+    },
+  },
+  mounted() {
+    this.bind()
+  },
   methods: {
-    add() {},
+    async add() {
+      if (this.newMessage != '') {
+        await db.collection('messages').add({
+          roomId: this.$route.params.id,
+          author: this.user.displayName,
+          message: this.newMessage,
+          createdAt: new Date(),
+        })
+        this.newMessage = ''
+      }
+    },
+    async bind() {
+      await this.$bind('room', db.collection('rooms').doc(this.$route.params.id))
+      await this.$bind('rooms', db.collection('rooms').orderBy('createdAt'))
+      await this.$bind(
+        'messages',
+        db
+          .collection('messages')
+          .where('roomId', '==', this.$route.params.id)
+          .orderBy('createdAt'),
+      )
+    },
   },
 }
 </script>
